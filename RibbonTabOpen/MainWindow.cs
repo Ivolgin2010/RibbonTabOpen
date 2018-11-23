edb25e86bfa37671b4a54b0a21e65785aeb55297
ivolgin0349 <ivolgin0349@gmail.com>
2018-11-23 10:18:33 +0300

    fixed dialog window

--- RibbonTabOpen/MainWindow.cs
+++ RibbonTabOpen/MainWindow.cs
@@ -44,5 +44,15 @@ namespace RibbonTabOpen
         {
            
         }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
