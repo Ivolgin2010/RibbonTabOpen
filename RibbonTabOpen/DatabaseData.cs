1663f7b8f87e3bddedaf3db2edb08e86a43100a6
ivolgin0349 <ivolgin0349@gmail.com>
2018-12-25 16:52:45 +0300

    add code to DatabaseData form

--- RibbonTabOpen/DatabaseData.cs
+++ RibbonTabOpen/DatabaseData.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace RibbonTabOpen
 {
     public partial class DatabaseData : Form
     {
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataReader dr;
+
         public DatabaseData()
         {
             InitializeComponent();
@@ -36,11 +34,19 @@ namespace RibbonTabOpen
 
         }
 
+        /// <summary>
+        ///  Открываем БД
+        /// </summary>
+        
         private void BtnOpen_Click(object sender, EventArgs e)
         {
-
+           
         }
 
+        /// <summary>
+        /// Удаляем строчку из БД
+        /// </summary>
+       
         private void BtnDelete_Click(object sender, EventArgs e)
         {
 
